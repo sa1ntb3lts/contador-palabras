718a2e8c2f90ff41619731a84c75290295ceef48
Santiago <229399078+sa1ntb3lts@users.noreply.github.com>
2026-09-15 16:28:51 -0700

    Added ContadorPalabras.java implementation

--- src/ContadorPalabras.java
+++ src/ContadorPalabras.java
@@ -1,1 +1,11 @@
+public class ContadorPalabras {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Uso: java ContadorPalabras <archivo>");
+            return;
+        }
 
+        String nombreArchivo = args[0];
+        System.out.println("Archivo: " + nombreArchivo);
+    }
+}
